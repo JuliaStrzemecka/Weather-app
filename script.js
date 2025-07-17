f30d905a3e3146992677a594418a25bf70731565
JuliaStrzemecka <juliastrzemecka067@gmail.com>
2025-07-17 14:01:16 +0200

    Add weather class and change css slightly

---
+++ script.js
@@ -0,0 +1,40 @@
+var apiKey = "38e63ea341c8aef4c9330681d6928e4c";
+
+var searchBtn = document.querySelector('#search-btn');
+
+var resultSection = document.getElementById("#result");
+
+class WeatherCity {
+
+    constructor(name, main, temp, feelsLike, country){
+        this.name = name;
+        this.temp = Math.round(temp);
+        this.main = main;
+        this.feelsLike = Math.round(feelsLike);
+        this.country = country;
+    }
+
+    render(){
+        const container = document.createElement('div');
+        container.innerHTML = `<h4>${this.name}</h4>
+            <p>${this.country}</p>
+            <span>${this.temperature}°C</span>
+            <p>Feels like ${this.feelsLike}°C<</p>`;
+
+        return container;
+    }
+}
+
+searchBtn.addEventListener("click", searchForWeather);
+
+document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault(); 
+    const city = document.querySelector("#City").value;
+    searchForWeather(city);
+});
+
+
+const searchForWeather = function() {
+    let city = document.querySelector('#City').value;
+    
+}
